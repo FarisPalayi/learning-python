924128ffe808822bb60616c19d5fbfa706978079
FarisPalayi <fariswritecode@gmail.com>
2021-07-15 23:06:54 +0530

    ➰ while loop

--- app.py
+++ app.py
@@ -12,19 +12,25 @@
 # print(round(-4.1))
 # print(abs(-3))  # always returns a positive value
 
-import math  # modules
+# import math  # modules
 
-print(math.ceil(4.4))
+# print(math.ceil(4.4))
 
-is_high_income = True
-has_good_credit = True
-has_criminal_record = False
+# is_high_income = True
+# has_good_credit = True
+# has_criminal_record = False
 
-if is_high_income and has_good_credit:
-  print("He is rich man")
+# if is_high_income and has_good_credit:
+#   print("He is rich man")
 
-if is_high_income or has_good_credit:
-  print("He is kinda rich")
+# if is_high_income or has_good_credit:
+#   print("He is kinda rich")
 
-if (is_high_income or has_good_credit) and not has_criminal_record:
-  print("yep, he is eligible I guess")
+# if (is_high_income or has_good_credit) and not has_criminal_record:
+#   print("yep, he is eligible I guess")
+
+i = 0
+while i <= 10:
+  print('*' * i)
+  i += 1
+print("done")
