09505eb6e33bd320489a01716387a0ba0c40408b
FarisPalayi <fariswritecode@gmail.com>
2021-07-16 00:13:32 +0530

    🅰 creating an f in terminal (me)

--- app.py
+++ app.py
@@ -94,6 +94,14 @@
 
 # print(price)
 
-for x in range(4):
-  for y in range(3):
-    print(f"({x}, {y})")
+# for x in range(4):
+#   for y in range(3):
+#     print(f"({x}, {y})")
+
+numbers = [5, 2, 5, 2, 2]
+
+for i in numbers:
+  f = ''
+  for j in range(i):
+    f += 'x'
+  print(f)
