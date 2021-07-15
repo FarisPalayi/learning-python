08cd4efaec19c85fbc7b3c9c6ba9b76efdcab9cb
FarisPalayi <fariswritecode@gmail.com>
2021-07-15 22:54:42 +0530

    ❓ if statement

--- app.py
+++ app.py
@@ -1,13 +1,27 @@
 # name = input("Type your name \n")
 # print(f"So your name is {name}")
 
-name = "john smith"
-print(name.lower())
-print(name.find('h'))  # search
-print(name.replace('john', 'drake'))
-print('john' in name)  # check to see if it includes
-print(10 / 3)
-print(10 // 3)  # returns an integer
-print(2 ** 3)  # power
-print(round(-4.1))
-print(abs(-3))  # always returns a positive value
+# name = "john smith"
+# print(name.lower())
+# print(name.find('h'))  # search
+# print(name.replace('john', 'drake'))
+# print('john' in name)  # check to see if it includes
+# print(10 / 3)
+# print(10 // 3)  # returns an integer
+# print(2 ** 3)  # power
+# print(round(-4.1))
+# print(abs(-3))  # always returns a positive value
+
+import math  # modules
+
+print(math.ceil(4.4))
+
+is_hot = False
+is_cold = True
+
+if is_hot:
+  print("wow it's hot")
+elif is_cold:
+  print("oo it's freezing out there")
+else:
+  print("not that hot")
